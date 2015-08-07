7030ad5d1f703ac6186115ca094889886ea152e8
Evan Clough <clough.evan@gmail.com>
2015-08-07 11:37:49 -0700

    Write integration spec to test that error message is returned what substring is not found in string.

--- spec/word_count_integration_spec.rb
+++ spec/word_count_integration_spec.rb
@@ -14,4 +14,12 @@ describe('the word count path', {:type => :feature}) do
     expect(page).to have_content("We found your word 2 times.")
   end
 
+  it('returns an error message when substring is not found in string.') do
+    visit('/')
+    fill_in('word', :with => 'burrito')
+    fill_in('sentence', :with => 'Tacocat is a cat that likes tacos.')
+    click_button('Search!')
+    expect(page).to have_content("We could not find your word! Please check your inputs.")
+  end
+
 end
