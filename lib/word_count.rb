ebeb3e79c51d1d85bcea9aebf8a0f03e2bb6dd7e
Evan Clough <clough.evan@gmail.com>
2015-08-07 09:58:18 -0700

    Add code to pass first spec. Does not pass.

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -1,7 +1,12 @@
 class String
 
-  define_method(:word_count) do
-    #code
+  define_method(:word_count) do |in_string|
+
+    for_string = self
+    times_found = in_string.scan(/for_string/).count
+
+    times_found > 0
+
   end
 
 end
--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -4,7 +4,7 @@ require('word_count')
 describe('String#word_count') do
 
   it('searches for a string within a string and returns true or false') do
-    expect("taco".word_count()).to(eq(true))
+    expect("taco".word_count("tacocat is a cat that likes tacos")).to(eq(true))
   end
 
 end
