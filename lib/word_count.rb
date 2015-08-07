0827d87391d247752719aa20c6c9b49ad6e5091d
Evan Clough <clough.evan@gmail.com>
2015-08-07 11:09:45 -0700

    Comment out logic in method that will be moved to file app.rb.

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -5,11 +5,11 @@ class String
     for_string = self.downcase
     times_found = in_string.downcase.scan(for_string).count
 
-    if times_found > 0
-      times_found
-    else
-      "We could not find your word in that sentence! Please check your inputs."
-    end
+    # if times_found > 0
+    #   times_found
+    # else
+    #   "We could not find your word in that sentence! Please check your inputs."
+    # end
 
   end
 
