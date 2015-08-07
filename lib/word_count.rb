78194b2fe3184c6423d45b8068f708988be10c04
Evan Clough <clough.evan@gmail.com>
2015-08-07 13:18:57 -0700

    Refactor code in method word_count. Remove single-use variable and insert variable definition into use case. Delete extraneous comment.

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -3,16 +3,8 @@ class String
   define_method(:word_count) do |in_string|
 
     unless self == ""
-      for_string = self.downcase
-      times_found = in_string.downcase.scan(for_string).count
+      times_found = in_string.downcase.scan(self.downcase).count
     end
-
-
-    # if times_found > 0
-    #   times_found
-    # else
-    #   "We could not find your word in that sentence! Please check your inputs."
-    # end
 
   end
 
