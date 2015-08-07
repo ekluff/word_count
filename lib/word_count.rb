de62d1300528b6ac4fc689b659a29376fb7b06cf
Evan Clough <clough.evan@gmail.com>
2015-08-07 10:28:48 -0700

    Add downcase method to variables defined in word_count. It will now match mixed case strings, e.g., 'Taco' will return a match for 'taco'.

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -2,8 +2,8 @@ class String
 
   define_method(:word_count) do |in_string|
 
-    for_string = self
-    times_found = in_string.scan(for_string).count
+    for_string = self.downcase
+    times_found = in_string.downcase.scan(for_string).count
 
     if times_found > 0
       times_found
