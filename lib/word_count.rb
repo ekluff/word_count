db475b71c5fa0a89ff81268591c05ebd224a6752
Evan Clough <clough.evan@gmail.com>
2015-08-07 13:14:52 -0700

    Surround content of method word_count with statement unless. Method word_count will now return null set if user defines method object as null set.

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -2,8 +2,11 @@ class String
 
   define_method(:word_count) do |in_string|
 
-    for_string = self.downcase
-    times_found = in_string.downcase.scan(for_string).count
+    unless self = ""
+      for_string = self.downcase
+      times_found = in_string.downcase.scan(for_string).count
+    end
+
 
     # if times_found > 0
     #   times_found
