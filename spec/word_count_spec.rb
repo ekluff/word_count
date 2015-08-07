b39554fd37276d14a6ff272ecaea707c66c4b832
Evan Clough <clough.evan@gmail.com>
2015-08-07 10:20:24 -0700

    Comment out first spec that tests word found as true or false. Now unnecessary.

--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -3,9 +3,9 @@ require('word_count')
 
 describe('String#word_count') do
 
-  it('searches for a string within a string and returns true or false') do
-    expect("taco".word_count("tacocat is a cat that likes tacos")).to(eq(true))
-  end
+  # it('searches for a string within a string and returns true or false') do
+  #   expect("taco".word_count("tacocat is a cat that likes tacos")).to(eq(true))
+  # end
 
   it('searches a string for a substring and returns the number of occurences') do
     expect("taco".word_count("tacocat is a cat that likes tacos")).to(eq(2))
