418f91fd8b68ff85d1967a840faa1e433f05d67f
Evan Clough <clough.evan@gmail.com>
2015-08-07 10:10:38 -0700

    Deleted debugging puts.

--- lib/word_count.rb
+++ lib/word_count.rb
@@ -2,16 +2,8 @@ class String
 
   define_method(:word_count) do |in_string|
 
-    puts in_string #Did the input string get input as a parameter?
-
     for_string = self
-
-    puts for_string #Did the substring get assigned properly?
-
     times_found = in_string.scan(for_string).count
-
-    puts in_string.scan(for_string) #Did the array of instances get created?
-    puts times_found #Did the times_found variable get properly assigned?
 
     times_found > 0
 
