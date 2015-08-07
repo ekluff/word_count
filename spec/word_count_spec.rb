8469d70acb0dfdb012e514cef5e2192a5beea8f7
Evan Clough <clough.evan@gmail.com>
2015-08-07 10:17:38 -0700

    Write third spec. It will test that logic returns a custom error message if substring is not found in string.

--- spec/word_count_spec.rb
+++ spec/word_count_spec.rb
@@ -11,4 +11,8 @@ describe('String#word_count') do
     expect("taco".word_count("tacocat is a cat that likes tacos")).to(eq(2))
   end
 
+  it('will return a custom error message if a substring is not found within a string') do
+    expect("burrito".word_count("tacocat is a cat that likes tacos")).to(eq("We could not find your word in that sentence! Please check your inputs."))
+  end
+
 end
