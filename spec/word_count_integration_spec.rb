9c2818717270d16174222954b854e043a1d814aa
Evan Clough <clough.evan@gmail.com>
2015-08-07 10:46:15 -0700

    Write first integration spec. It will search for 'TaCo' in 'Tacocat is a cat that likes tacos.' and test that program returns 'Your word was found 2 times.'

--- spec/word_count_integration_spec.rb
+++ spec/word_count_integration_spec.rb
@@ -3,3 +3,15 @@ require('launchy')
 require('./app')
 Capybara.app = Sinatra::Application
 set(:show_exceptions, false)
+
+describe('the word count path', {:type => :feature}) do
+
+  it('searches for a user-input substring in a user-input string') do
+    visit('/')
+    fill_in('word', :with => 'TaCo')
+    fill_in('sentence', :with => 'Tacocat is a cat that likes tacos.')
+    click_button('Send')
+    expect(page).to have_content("Your word was found 2 times.")
+  end
+
+end
